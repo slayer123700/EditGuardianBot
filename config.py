4cb43421bcf8f1b601003807a981da35cb3261c1
slayer <meerutuniversity996@gmail.com>
2025-05-30 10:18:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ API_ID = "4014305"
 API_HASH = "b0cb9e17b2b8bcde3be7161c8bfa6013"
 
 # Telegram bot token
-TELEGRAM_TOKEN = ""
+TELEGRAM_TOKEN = "7671461535:AAGOPau3s4b6g2pbJm5sRz2iqbhN0K5tfbA"
 
 # Your telegram user ID (owner)
-OWNER_ID = 5817544006
+OWNER_ID = 6018803920
 
-SUDO_ID = [1669178360, 7642159940, 6128266949]
+SUDO_ID = [6018803920]
 
 MONGO_URI = "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority"
 
@@ -18,4 +18,4 @@ LOGGER = True
 
 BOT_NAME = "Edit Guardian"
 
-SUPPORT_ID = (-1001515594503)
+SUPPORT_ID = (-1002392274240)
